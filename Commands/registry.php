a3479d1606958c4eb2836d420764d65e7d5a113f
Kohagura Maimu <e225711@ie.u-ryukyu.ac.jp>
2024-05-16 17:15:34 +0900

    registry.php require filepath fixed

--- Commands/registry.php
+++ Commands/registry.php
@@ -1,4 +1,8 @@
 <?php
+require_once __DIR__ . '/Programs/Migrate.php';
+require_once __DIR__ . '/Programs/CodeGeneration.php';
+require_once __DIR__ . '/Programs/Seed.php';
+
 return [
     Commands\Programs\Migrate::class,
     Commands\Programs\CodeGeneration::class,
